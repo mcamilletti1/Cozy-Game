14b3411f5736ba3c98d21b2467622673e770bb38
Margaret Camilletti <mhcamilletti@gmail.com>
2023-12-19 12:34:50 -0500

    rendered game map zoomed in

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-const c  = canvas.getContext('2d');
-
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 c.fillStyle = 'white';
-
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = './My Game Assets/Cozy Game.png';
+console.log(image);
+
+
+image.onload = () => {
+    c.drawImage(image, -600, -170);
+}
+
